12d43880c05346ff23ca437d1b2eb5a783f8e797
skanda <skanda.vivek@gmail.com>
2023-07-18 14:03:28 -0400

    Update chat.py

--- app/chat.py
+++ app/chat.py
@@ -1,14 +1,35 @@
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Weaviate
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain import OpenAI
+
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
 
 import streamlit as st
 
 @st.cache_data
 def chat_with_pdf(_text, _embeddings, chat_prompt):
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.split_text(_text)
+
+    system_template="""Use the following pieces of context to answer the users question. 
+
+
+    Begin!
+    ----------------
+    {summaries}
+    """
+
+    messages = [
+        SystemMessagePromptTemplate.from_template(system_template),
+        HumanMessagePromptTemplate.from_template("{question}")
+    ]
+    prompt = ChatPromptTemplate.from_messages(messages)
+
 
     docsearch = Weaviate.from_texts(
         texts,
@@ -18,9 +39,23 @@ def chat_with_pdf(_text, _embeddings, chat_prompt):
         metadatas=[{"source": f"{i}-pl"} for i in range(len(texts))],
     )
 
-    chain = RetrievalQAWithSourcesChain.from_chain_type(
-        OpenAI(temperature=0), chain_type="stuff", retriever=docsearch.as_retriever()
-    )
+    def get_chain(store):
+        chain_type_kwargs = {"prompt": prompt}
+        chain = RetrievalQAWithSourcesChain.from_chain_type(
+            OpenAI(temperature=0), 
+            chain_type="stuff", 
+            retriever=store.as_retriever(),
+            chain_type_kwargs=chain_type_kwargs,
+            reduce_k_below_max_tokens=True
+        )
+        return chain
+    
+    chain = get_chain(docsearch)
+
+
+    # chain = RetrievalQAWithSourcesChain.from_chain_type(
+    #     OpenAI(temperature=0), chain_type="stuff", retriever=docsearch.as_retriever()
+    # )
 
     response = chain(
         {"question": chat_prompt},
